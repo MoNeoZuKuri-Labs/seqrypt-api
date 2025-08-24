6cc743efa3e2ecb981ca329911126975757f276f
Mahdeen Khan <mahdeen.s.khan@yale.edu>
2025-08-23 20:16:25 -0400

    pocketbase attempt

---
+++ pocket.go
@@ -0,0 +1,66 @@
+package pocket
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+func pocket() {
+	log.SetPrefix("ERR: ")
+	log.SetFlags(0)
+
+	app := pocketbase.New()
+
+	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+		// serves static files from the provided public dir (if exists)
+		se.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
+
+		return se.Next()
+	})
+
+	app.OnRecordCreate().BindFunc(func(e *core.RecordEvent) error {
+		log.Printf("Record content %v", e.Record.FieldsData()["password"])
+
+		return e.Next()
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func mdHashing(input string) string {
+	byteInput := []byte(input)
+	md5Hash := md5.Sum(byteInput)
+	return hex.EncodeToString(md5Hash[:]) // by referring to it as a string
+}
+
+func encryptString(value []byte, keyPhrase string) []byte {
+	aesBlock, err := aes.NewCipher([]byte(mdHashing(keyPhrase)))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	gcmInstance, err := cipher.NewGCM(aesBlock)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	nonce := make([]byte, gcmInstance.NonceSize())
+	_, _ = io.ReadFull(rand.Reader, nonce)
+
+	cipheredText := gcmInstance.Seal(nonce, nonce, value, nil)
+
+	return cipheredText
+}
